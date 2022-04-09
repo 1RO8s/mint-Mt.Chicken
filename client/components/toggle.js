d1335d340e43cc692b05c6ac25366ada019d0d74
1ro8s <saki0220hiro@gmail.com>
2022-04-10 06:49:11 +0900

    トグルのラベル位置変更

--- client/components/toggle.js
+++ client/components/toggle.js
@@ -3,15 +3,16 @@
 const Toggle = ({text}) => {
   const msg = text || "[message]"
   return (
-    <label class="flex items-center cursor-pointer mx-3 my-3">
-    <div class="relative">
-      <input type="checkbox" class="sr-only"/>
-      <div class="block bg-gray-600 w-14 h-8 rounded-full"></div>
-      <div class="dot absolute left-1 top-1 bg-white w-6 h-6 rounded-full transition"></div>
+    <label className="flex flex-col items-center cursor-pointer mx-3 my-3">
+      <div className="ml-3 text-gray-700 font-medium">
+        {msg}
+      </div>
+    <div className="relative">
+      <input type="checkbox" className="sr-only"/>
+      <div className="block bg-gray-600 w-14 h-8 rounded-full"></div>
+      <div className="dot absolute left-1 top-1 bg-white w-6 h-6 rounded-full transition"></div>
     </div>
-    <div class="ml-3 text-gray-700 font-medium">
-      {msg}
-    </div>
+    
     </label>
   )
 }
