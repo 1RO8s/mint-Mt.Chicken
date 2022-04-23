e989968d13e3137057e4327bb22e8179ea7cb6d6
1ro8s <saki0220hiro@gmail.com>
2022-04-24 07:17:07 +0900

    state切り替えの機能追加

--- client/components/toggle.js
+++ client/components/toggle.js
@@ -1,4 +1,10 @@
-const Toggle = ({ title, ruby }) => {
+const Toggle = ({ title, ruby, state, setState }) => {
+  
+  const switchToggle = () => {
+    setState(!state)
+    console.log(`## state:${state}`)
+  }
+
   return (
     <label className="mx-3 my-3 flex cursor-pointer flex-col items-center">
       <div className="flex flex-col ml-3 font-medium text-gray-700">
@@ -6,7 +12,7 @@ const Toggle = ({ title, ruby }) => {
         <span className="text-sm">{title}</span>
       </div>
       <div className="relative">
-        <input type="checkbox" className="sr-only" />
+        <input type="checkbox" className="sr-only" onClick={switchToggle}/>
         <div className="block h-8 w-14 rounded-full bg-gray-600"></div>
         <div className="dot absolute left-1 top-1 h-6 w-6 rounded-full bg-white transition"></div>
       </div>
