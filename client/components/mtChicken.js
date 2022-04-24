605a1e24670bc8c9bdc8ed2547b660e0388f784b
1ro8s <saki0220hiro@gmail.com>
2022-04-25 04:48:25 +0900

    refの追加

--- client/components/mtChicken.js
+++ client/components/mtChicken.js
@@ -25,7 +25,7 @@ const MtChicken = ({
   //bgColor = "#16adff"
 
   return (
-    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1000 1000">
+    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1000 1000" ref={ref}>
   <defs>
     <style>{`
       .outline {
