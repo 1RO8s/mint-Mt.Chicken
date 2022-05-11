d83dd31a1b9d94fe3d59f18f4eb340e8acab9592
1ro8s <saki0220hiro@gmail.com>
2022-05-11 17:11:58 +0900

    クラスコンポーネントに変更

--- client/components/color-picker.js
+++ client/components/color-picker.js
@@ -1,58 +1,75 @@
-import InputColor from 'react-input-color'
 import { ChromePicker } from 'react-color'
+import { SketchPicker } from 'react-color'
 import React from 'react'
+import reactCSS from 'reactcss'
 
-const ColorPicker = ({ title, ruby, color, setColor, initialValue }) => {
-  //const _title = title || '_title_'
+class ColorPicker extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      displayColorPicker: false,
+    }
+  }
+  handleClick = () =>
+    this.setState({ displayColorPicker: !this.state.displayColorPicker })
+  handleClose = () => this.setState({ displayColorPicker: false })
+  handleChange = (_color) => this.props.setColor(_color.hex)
 
-  const [isOpen, setIsOpen] = React.useState(false)
+  render() {
+    const { title, ruby } = this.props
 
+    const styles = reactCSS({
+      default: {
+        color: {
+          width: '90px',
+          height: '30px',
+          borderRadius: '2px',
+          background: this.props.color,
+        },
+        swatch: {
+          padding: '1px',
+          background: '#fff',
+          borderRadius: '1px',
+          boxShadow: '0 0 0 1px rgba(0,0,0,.1)',
+          display: 'inline-block',
+          cursor: 'pointer',
+        },
+        popover: {
+          position: 'absolute',
+          zIndex: '2',
+        },
+        cover: {
+          position: 'fixed',
+          top: '0px',
+          right: '0px',
+          bottom: '0px',
+          left: '0px',
+        },
+      },
+    })
 
-  const handleChange = (color) => {
-    setColor(color.hex);
-  };
-
-  const handleOpen = () => {
-    setIsOpen(!isOpen);
-  };
-  const handleClose = () => {
-    setIsOpen(false);
-  };
-
-
-  const Picker = () => (
-    <div className='popover z-10 absolute'>
-      <div className='cover fixed top-0 right-0 left-0 bottom-0' onClick={handleClose}/>
-      <ChromePicker color={color} onChange={setColor} />
-    </div>
-  )
-
-  
-
-  
-  return (
-    <div className="-grow mx-1 my-1 flex flex-col items-center text-left">
-      <div className="flex flex-col">
-        <span className="text-xs">{ruby}</span>
-        <span className="text-sm">{title}</span>
-        <InputColor
-          initialValue={initialValue}
-          onChange={setColor}
-          style={{ height: 30, width: 90, padding: 0 }}
-          placement="left"
-        />
+    return (
+      <div className="mx-1 my-1 flex flex-col items-left text-left">
+        <div className="flex flex-col mb-1">
+          <span className="text-xs">{ruby}</span>
+          <span className="text-sm font-bold">{title}</span>
+        </div>
+        <div>
+          <div style={styles.swatch} onClick={this.handleClick}>
+            <div style={styles.color} />
+          </div>
+          {this.state.displayColorPicker ? (
+            <div style={styles.popover}>
+              <div style={styles.cover} onClick={this.handleClose} />
+              <ChromePicker
+                color={this.props.color}
+                onChangeComplete={this.handleChange}
+              />
+            </div>
+          ) : null}
+        </div>
       </div>
-    </div>
-  )
-
-  return (
-    <div className="-grow mx-1 my-1 flex flex-col items-center text-left">
-      <div className="flex flex-col">
-        <span className="text-xs">{ruby}</span>
-        <span className="text-sm">{title}</span>
-        <ChromePicker color={color} onChange={setColor} />
-      </div>
-    </div>
-  )
+    )
+  }
 }
 export default ColorPicker
