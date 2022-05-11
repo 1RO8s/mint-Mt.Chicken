8c0dfec26b37a382b2a429c8bdf7d50f7329f9b2
1ro8s <saki0220hiro@gmail.com>
2022-05-11 18:00:58 +0900

    色々変更

--- client/pages/home.jsx
+++ client/pages/home.jsx
@@ -18,11 +18,26 @@ const nftContractAddress = '0x865ccbfe3cac3ce0834c006c2581c08fd5ebc468'
 const opensea = 'https://testnets.opensea.io/assets/mumbai' // testnet
 //const opensea = 'https://opensea.io/assets/matic'
 
+const chains = {
+  main: { name: 'matic', id: '' },
+  test: { name: 'mumbai', id: '0x13881' },
+}
+
+const chain = chains.test
 
 const Home = () => {
   //let accounts;
   //const isConneted = accounts == undefined;
 
+  const isConnect = () => {
+    const { ethereum } = window
+    if (!ethereum) {
+      alert('Metamaskが見つかりませんでした')
+      return
+    } else {
+      console.log()
+    }
+  }
 
   const connectWallet = async () => {
     const { ethereum } = window
@@ -40,7 +55,8 @@ const Home = () => {
     const mumbaiChainId = '0x13881'
 
     if (chainId !== mumbaiChainId) {
-      alert('You are not connected to the Mumbai Testnet!')
+      //alert('You are not connected to the Mumbai Testnet!')
+      alert(`${chain.name}チェーンに接続してください`)
       return
     }
 
@@ -78,51 +94,45 @@ const Home = () => {
       console.log(signer)
 
       const hexToInt = (s) => parseInt(s, 16)
-      console.log('## colors:',colors)
-      console.log('## colors.replace:',[
-        hexToInt(outlineColor.hex.slice(1,7)), // outline
-        hexToInt(tosakaColor.hex.slice(1,7)), // cockscomb
-        hexToInt(eyeColor.hex.slice(1,7)), // eyes
-        hexToInt(headColor.hex.slice(1,7)),  // face
-        hexToInt(headShadowColor.hex.slice(1,7)), // face shadow
-        hexToInt(bodyColor.hex.slice(1,7)), // body
-        hexToInt(bodyShadowColor.hex.slice(1,7)), // body shadow
-        hexToInt(tailColor.hex.slice(1,7)), // tail
-        hexToInt(tailShadowColor.hex.slice(1,7)), // tail shadow
-        hexToInt(moustacheColor.hex.slice(1,7)), // wattle
-        hexToInt(beakColor.hex.slice(1,7)), // beak
-        hexToInt(footColor.hex.slice(1,7)), // foot
+      console.log('## colors:', colors)
+      console.log('## colors.replace:', [
+        hexToInt(outlineColor.hex.slice(1, 7)), // outline
+        hexToInt(tosakaColor.hex.slice(1, 7)), // cockscomb
+        hexToInt(eyeColor.hex.slice(1, 7)), // eyes
+        hexToInt(headColor.hex.slice(1, 7)), // face
+        hexToInt(headShadowColor.hex.slice(1, 7)), // face shadow
+        hexToInt(bodyColor.hex.slice(1, 7)), // body
+        hexToInt(bodyShadowColor.hex.slice(1, 7)), // body shadow
+        hexToInt(tailColor.hex.slice(1, 7)), // tail
+        hexToInt(tailShadowColor.hex.slice(1, 7)), // tail shadow
+        hexToInt(moustacheColor.hex.slice(1, 7)), // wattle
+        hexToInt(beakColor.hex.slice(1, 7)), // beak
+        hexToInt(footColor.hex.slice(1, 7)), // foot
       ])
 
-      
       let nftTx = await nftContract.mint(
         [
-          hexToInt(outlineColor.hex.slice(1,7)), // outline
-          hexToInt(tosakaColor.hex.slice(1,7)), // cockscomb
-          hexToInt(eyeColor.hex.slice(1,7)), // eyes
-          hexToInt(headColor.hex.slice(1,7)),  // face
-          hexToInt(headShadowColor.hex.slice(1,7)), // face shadow
-          hexToInt(bodyColor.hex.slice(1,7)), // body
-          hexToInt(bodyShadowColor.hex.slice(1,7)), // body shadow
-          hexToInt(tailColor.hex.slice(1,7)), // tail
-          hexToInt(tailShadowColor.hex.slice(1,7)), // tail shadow
-          hexToInt(moustacheColor.hex.slice(1,7)), // wattle
-          hexToInt(beakColor.hex.slice(1,7)), // beak
-          hexToInt(footColor.hex.slice(1,7)), // foot
+          hexToInt(outlineColor.hex.slice(1, 7)), // outline
+          hexToInt(tosakaColor.hex.slice(1, 7)), // cockscomb
+          hexToInt(eyeColor.hex.slice(1, 7)), // eyes
+          hexToInt(headColor.hex.slice(1, 7)), // face
+          hexToInt(headShadowColor.hex.slice(1, 7)), // face shadow
+          hexToInt(bodyColor.hex.slice(1, 7)), // body
+          hexToInt(bodyShadowColor.hex.slice(1, 7)), // body shadow
+          hexToInt(tailColor.hex.slice(1, 7)), // tail
+          hexToInt(tailShadowColor.hex.slice(1, 7)), // tail shadow
+          hexToInt(moustacheColor.hex.slice(1, 7)), // wattle
+          hexToInt(beakColor.hex.slice(1, 7)), // beak
+          hexToInt(footColor.hex.slice(1, 7)), // foot
         ],
         //[false, true, false, false]
-        [
-          hasForehead,
-          hasNose,
-          hasCheek,
-          hasBerry
-        ]
+        [hasForehead, hasNose, hasCheek, hasBerry]
       )
       console.log('Mining....', nftTx.hash)
-      console.log("hexToInt('999999'):",hexToInt('999999'))
+      console.log("hexToInt('999999'):", hexToInt('999999'))
       setMiningStatus(0)
-      console.log('1.miningStatus:',miningStatus)
-      
+      console.log('1.miningStatus:', miningStatus)
+
       // wait()が終わるまでローディング表示
       setIsLoading(true)
 
@@ -131,7 +141,7 @@ const Home = () => {
       setLoadingState(1)
       setMiningStatus(1)
       setIsLoading(false)
-      console.log('2.miningStatus:',miningStatus)
+      console.log('2.miningStatus:', miningStatus)
 
       let event = tx.events[0]
       let value = event.args[2]
@@ -145,27 +155,28 @@ const Home = () => {
     }
   }
 
-
   // カラー設定
-  const [outlineColor, setOutlineColor] = React.useState({})
-  const [tosakaColor, setTosakaColor] = React.useState({})
-  const [headColor, setHeadColor] = React.useState({})
-  const [headShadowColor, setHeadShadowColor] = React.useState({})
-  const [eyeColor, setEyeColor] = React.useState({})
-  const [beakColor, setBeakColor] = React.useState({})
-  const [bodyColor, setBodyColor] = React.useState({})
-  const [bodyShadowColor, setBodyShadowColor] = React.useState({})
-  const [footColor, setFootColor] = React.useState({})
-  const [moustacheColor, setMoustacheColor] = React.useState({})
-  const [tailColor, setTailColor] = React.useState({})
-  const [tailShadowColor, setTailShadowColor] = React.useState({})
+  const [outlineColor, setOutlineColor] = React.useState('#999999')
+  const [tosakaColor, setTosakaColor] = React.useState('#b3b3b3')
+  const [headColor, setHeadColor] = React.useState('#ffffff')
+  const [headShadowColor, setHeadShadowColor] = React.useState('#cccccc')
+  const [eyeColor, setEyeColor] = React.useState('#999999')
+  const [beakColor, setBeakColor] = React.useState('#cccccc')
+  const [bodyColor, setBodyColor] = React.useState('#e6e6e6')
+  const [bodyShadowColor, setBodyShadowColor] = React.useState('#b3b3b3')
+  const [footColor, setFootColor] = React.useState('#cccccc')
+  const [moustacheColor, setMoustacheColor] = React.useState('#b3b3b3')
+  const [tailColor, setTailColor] = React.useState('#ffffff')
+  const [tailShadowColor, setTailShadowColor] = React.useState('#cccccc')
 
   const [hasForehead, setForehead] = React.useState(false)
   const [hasNose, setNose] = React.useState(false)
   const [hasCheek, setCheek] = React.useState(false)
   const [hasBerry, setBerry] = React.useState(false)
 
-  // 
+  const mtChickenElement = React.useRef(null)
+
+  //
   const colors = {
     outlineColor,
     tosakaColor,
@@ -181,49 +192,53 @@ const Home = () => {
     tailShadowColor,
   }
 
-  const scribbles= {
+  const scribbles = {
     hasForehead,
     hasNose,
     hasCheek,
-    hasBerry
+    hasBerry,
   }
-
 
   console.log('colors:')
   console.log(colors)
 
   // ランダム機能
   const randomColor = () => {
-    return '#' + Math.floor(Math.random() * 16777215).toString(16)
+    return (
+      '#' +
+      Math.floor(Math.random() * 16777215)
+        .toString(16)
+        .padStart(6, '0')
+    )
   }
   const setRandomColor = () => {
-    setOutlineColor({ hex: randomColor() })
-    setTosakaColor({ hex: randomColor() })
-    setHeadColor({ hex: randomColor() })
-    setHeadShadowColor({ hex: randomColor() })
-    setEyeColor({ hex: randomColor() })
-    setBeakColor({ hex: randomColor() })
-    setBodyColor({ hex: randomColor() })
-    setBodyShadowColor({ hex: randomColor() })
-    setFootColor({ hex: randomColor() })
-    setMoustacheColor({ hex: randomColor() })
-    setTailColor({ hex: randomColor() })
-    setTailShadowColor({ hex: randomColor() })
+    setOutlineColor(randomColor())
+    setTosakaColor(randomColor())
+    setHeadColor(randomColor())
+    setHeadShadowColor(randomColor())
+    setEyeColor(randomColor())
+    setBeakColor(randomColor())
+    setBodyColor(randomColor())
+    setBodyShadowColor(randomColor())
+    setFootColor(randomColor())
+    setMoustacheColor(randomColor())
+    setTailColor(randomColor())
+    setTailShadowColor(randomColor())
   }
 
   // Modal実装
   const [showModal, setShowModal] = React.useState(false)
-  
+
   const openModal = async () => {
-    await mintChicken()
+    //await mintChicken()
     //alert('minted')
-    console.log('3.miningStatus:',miningStatus)
-    if(miningStatus==1){
+    setMiningStatus(1)
+    console.log('3.miningStatus:', miningStatus)
+    if (miningStatus == 1) {
       setShowModal(true)
     } else {
       setShowModal(false)
     }
-    
   }
 
   return (
@@ -239,10 +254,14 @@ const Home = () => {
         />
       </Head>
       <div className="flex h-screen flex-col  items-center justify-center py-2">
-        <header className="flex-between flex w-full flex-row justify-between" style={{borderBottom: "solid 10px black"}}>
+        <header
+          className="flex-between flex w-full flex-row justify-between"
+          style={{ borderBottom: 'solid 10px black' }}
+        >
           <h2 className="m-auto mx-28 align-middle font-lily text-3xl text-sky-400">
             Mt.Chicken
           </h2>
+          <div className="top-circle"></div>
           <button
             className="m-5 rounded bg-blue-500 py-2 px-4 font-bold text-white hover:bg-blue-400"
             onClick={connectWallet}
@@ -254,128 +273,153 @@ const Home = () => {
           </button>
         </header>
         <main className="bg-main-color grow-1 flex w-full flex-1 flex-col justify-center overflow-y-scroll px-20 text-center sm:flex-row">
-          <div
-            id="MtChicken"
-            className="flex h-max w-full justify-center sm:w-1/2"
-          >
+          <div id="MtChicken" className="flex h-max w-1/2 flex-col">
             <MtChicken
-              outlineColor={outlineColor}
-              headColor={headColor}
-              headShadowColor={headShadowColor}
-              eyeColor={eyeColor}
-              tailColor={tailColor}
-              tailShadowColor={tailShadowColor}
-              beakColor={beakColor}
-              tosakaColor={tosakaColor}
-              footColor={footColor}
-              moustacheColor={moustacheColor}
-              bodyColor={bodyColor}
-              bodyShadowColor={bodyShadowColor}
-              hasForehead={hasForehead}
-              hasCheek={hasCheek}
-              hasNose={hasNose}
-              hasBerry={hasBerry}
-              bgColor={"#16adff"}
+              {...colors}
+              {...scribbles}
+              bgColor={'#16adff'}
+              ref={mtChickenElement}
             />
           </div>
-          <div id="color-pickers" className="w-1/2 overflow-y-scroll pt-3 h-full">
+          <div
+            id="color-pickers"
+            className="h-full w-1/2 overflow-y-scroll pt-3"
+          >
             <div className="m-3 rounded-xl bg-white p-3">
-              <h2 className="m-1 text-left text-xl font-bold">色選択</h2>
+              <h2 className="m-1 text-left text-xl font-bold">
+                <span>色選択</span>
+                <span className="mx-2 text-sm">Color Select</span>
+              </h2>
               <hr style={{ border: 'solid 1px black' }} />
               <div className="flex flex-wrap">
                 <ColorPicker
                   title="アウトライン"
                   ruby="Outline"
+                  color={outlineColor}
                   setColor={setOutlineColor}
                   initialValue="#999"
                 />
                 <ColorPicker
                   title="とさか"
                   ruby="Cockscomb"
+                  color={tosakaColor}
                   setColor={setTosakaColor}
                   initialValue="#b3b3b3"
                 />
                 <ColorPicker
                   title="め"
                   ruby="Eyes"
+                  color={eyeColor}
                   setColor={setEyeColor}
                   initialValue="#999"
                 />
                 <ColorPicker
                   title="かお"
                   ruby="Face"
+                  color={headColor}
                   setColor={setHeadColor}
                   initialValue="#fff"
                 />
                 <ColorPicker
                   title="かおの影"
                   ruby="Face shadow"
+                  color={headShadowColor}
                   setColor={setHeadShadowColor}
                   initialValue="#ccc"
                 />
                 <ColorPicker
                   title="からだ"
                   ruby="Body"
+                  color={bodyColor}
                   setColor={setBodyColor}
                   initialValue="#e6e6e6"
                 />
                 <ColorPicker
                   title="からだの影"
                   ruby="Body shadow"
+                  color={bodyShadowColor}
                   setColor={setBodyShadowColor}
                   initialValue="#b3b3b3"
                 />
                 <ColorPicker
                   title="しっぽ"
                   ruby="Tail"
+                  color={tailColor}
                   setColor={setTailColor}
                   initialValue="#fff"
                 />
                 <ColorPicker
                   title="しっぽの影"
                   ruby="Tail shadow"
+                  color={tailShadowColor}
                   setColor={setTailShadowColor}
                   initialValue="#ccc"
                 />
                 <ColorPicker
                   title="にくぜん"
                   ruby="Wattle"
+                  color={moustacheColor}
                   setColor={setMoustacheColor}
                   initialValue="#b3b3b3"
                 />
                 <ColorPicker
                   title="くちばし"
                   ruby="Beak"
+                  color={beakColor}
                   setColor={setBeakColor}
                   initialValue="#ccc"
                 />
                 <ColorPicker
                   title="あし"
                   ruby="Foot"
+                  color={footColor}
                   setColor={setFootColor}
                   initialValue="#ccc"
                 />
               </div>
             </div>
             <div className="m-3 rounded-xl bg-white p-3">
-              <h2 className="m-1 text-left text-xl font-bold">らくがき選択</h2>
+              <h2 className="m-1 text-left text-xl font-bold">
+                <span>らくがき選択</span>
+                <span className="mx-2 text-sm">Doodle Select</span>
+              </h2>
               <hr></hr>
               <div className="flex flex-row">
-                <Toggle title="額の傷" ruby="forehead" state={hasForehead} setState={setForehead}/>
-                <Toggle title="頬の傷" ruby="cheek" state={hasCheek} setState={setCheek}/>
-                <Toggle title="ひげ" ruby="nose" state={hasNose} setState={setNose}/>
-                <Toggle title="腹の傷" ruby="belly" state={hasBerry} setState={setBerry}/>
+                <Toggle
+                  title="額の傷"
+                  ruby="forehead"
+                  state={hasForehead}
+                  setState={setForehead}
+                />
+                <Toggle
+                  title="頬の傷"
+                  ruby="cheek"
+                  state={hasCheek}
+                  setState={setCheek}
+                />
+                <Toggle
+                  title="鼻毛"
+                  ruby="nose"
+                  state={hasNose}
+                  setState={setNose}
+                />
+                <Toggle
+                  title="腹の傷"
+                  ruby="belly"
+                  state={hasBerry}
+                  setState={setBerry}
+                />
               </div>
             </div>
             <div className="my-4 flex flex-row px-1">
-            <button
-              className="mx-3 flex w-fit place-content-center rounded bg-sky-400 border-sky-200 border-2 py-2 px-4 font-bold"
-              onClick={setRandomColor}
-            >
-              <span className="flex">
-                <span className="mx-1 flex text-white">ランダム</span>
-              </span>
-            </button>
+              <button
+                className="mx-3 flex w-fit place-content-center rounded border-2 border-sky-200 bg-sky-400 py-2 px-4 font-bold"
+                onClick={setRandomColor}
+              >
+                <span className="flex">
+                  <span className="mx-1 flex text-white">ランダム</span>
+                </span>
+              </button>
               <button
                 className="mx-3 flex w-1/2 place-content-center rounded bg-yellow-300 py-2 px-4 font-bold"
                 onClick={openModal}
@@ -414,9 +458,7 @@ const Home = () => {
         scribbles={scribbles}
         tokenId={newItemId}
       />
-      <Loading
-        isLoading={isLoading}
-      />
+      <Loading isLoading={isLoading} />
     </>
   )
 }
