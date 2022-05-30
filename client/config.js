2e9628c69c5a3c37e1f521c218c4cf6538ced291
1RO8s@kizzo168 <saki0220hiro+git@gmail.com>
2022-05-31 03:51:04 +0900

    メインネット用に修正

--- client/config.js
+++ client/config.js
@@ -1,6 +1,6 @@
 import nft from './utils/MtChickenNFT.json'
 
-export const CONTRACT_ADDRESS = '0x865ccbfe3cac3ce0834c006c2581c08fd5ebc468';
-export const OPENSEA_URL = 'https://testnets.opensea.io/assets/mumbai'
+export const CONTRACT_ADDRESS = '0x0057a7c625352190d27d9614ee09fac999f8c93d';
+export const OPENSEA_URL = 'https://opensea.io/assets/matic'
 export const NFT = nft;
-export const CHAIN = { name:'Mumbai',id:'0x13881' }
+export const CHAIN = { name:'MATIC',id:'0x89' }
