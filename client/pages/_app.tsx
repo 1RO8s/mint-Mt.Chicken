20ed5ddaf2683ed8460d9a8d6d7fce15a2ecc90b
1ro8s <saki0220hiro@gmail.com>
2022-06-20 07:50:34 +0900

    scriptタグの修正

--- client/pages/_app.tsx
+++ client/pages/_app.tsx
@@ -1,7 +1,7 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
 import Head from 'next/head'
-import { useRouter } from "next/router";
+import { useRouter } from 'next/router'
 //import GoogleAnalytics from '../components/GoogleAnalytics'
 import Script from 'next/script'
 import { useEffect } from 'react'
@@ -10,20 +10,10 @@ import { existsGaId, pageview, GA_TRACKING_ID } from '../utils/gtag'
 function MyApp({ Component, pageProps }: AppProps) {
   const router = useRouter()
 
-  console.log('existsGaId:',existsGaId)
-
-  console.log("NEXT_PUBLIC_VERCEL_ENV:",process.env.NEXT_PUBLIC_VERCEL_ENV)
-  console.log("NODE_ENV:",process.env.NODE_ENV)
-  console.log("VERCEL_ENV:",process.env.VERCEL_ENV)
-  console.log("GA_TRACKING_ID:",process.env.GA_TRACKING_ID)
-  console.log("NEXT_GA_TRACKING_ID:",process.env.NEXT_GA_TRACKING_ID)
-  console.log("NEXT_PUBLIC_GA_TRACKING_ID:",process.env.NEXT_PUBLIC_GA_TRACKING_ID)
-  console.log("NEXT_PUBLIC_VERCEL_GA_TRACKING_ID:",process.env.NEXT_PUBLIC_VERCEL_GA_TRACKING_ID)
-
   useEffect(() => {
     if (!existsGaId) return
 
-    const handleRouteChange = (url : String) => {
+    const handleRouteChange = (url: String) => {
       pageview(url)
     }
     router.events.on('routeChangeComplete', handleRouteChange)
@@ -40,7 +30,11 @@ function MyApp({ Component, pageProps }: AppProps) {
         <title>Mt.Chicken</title>
         <link rel="icon" href="/mt-chicken.png" />
         <link rel="preconnect" href="https://fonts.googleapis.com" />
-        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="true" />
+        <link
+          rel="preconnect"
+          href="https://fonts.gstatic.com"
+          crossOrigin="true"
+        />
         <link
           href="https://fonts.googleapis.com/css2?family=Lily+Script+One&family=M+PLUS+1:wght@400;500;700&family=Ubuntu:wght@400;700&display=swap"
           rel="stylesheet"
@@ -48,22 +42,24 @@ function MyApp({ Component, pageProps }: AppProps) {
         <meta property="ga-id" content={GA_TRACKING_ID} />
         <meta property="gais" content={`gais-${existsGaId}`} />
         {existsGaId && (
-        <>
-          <Script
-            defer
-            src={`https://www.googletagmanager.com/gtag/js?id=${GA_TRACKING_ID}`}
-            strategy="afterInteractive"
-          />
-          <Script id="ga" defer strategy="afterInteractive">
-            {`
-              window.dataLayer = window.dataLayer || [];
-              function gtag(){dataLayer.push(arguments);}
-              gtag('js', new Date());    
-              gtag('config', '${GA_TRACKING_ID}');
-          `}
-          </Script>
-        </>
-      )}
+          <>
+            <script
+              async
+              src={`https://www.googletagmanager.com/gtag/js?id=${GA_TRACKING_ID}`}
+            />
+            <script
+              dangerouslySetInnerHTML={{
+                __html: `
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+          gtag('config', '${GA_TRACKING_ID}', {
+            page_path: window.location.pathname,
+          });`,
+              }}
+            />
+          </>
+        )}
       </Head>
       <Component {...pageProps} />
     </>
