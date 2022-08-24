a90d8c922fc77e7b80aeccc2e76e82e42d81df62
1ro8s <saki0220hiro@gmail.com>
2022-08-25 05:11:25 +0900

    コンポーネントとGAの修正

--- client/components/GoogleAnalytics.js
+++ client/components/GoogleAnalytics.js
@@ -1,22 +1,27 @@
 import Script from 'next/script'
 import { existsGaId, GA_TRACKING_ID } from '../utils/gtag'
 
-const GoogleAnalytics = () => (
-  <>
-    {existsGaId && (
-      <>
-        <Script defer src={`https://www.googletagmanager.com/gtag/js?id=${GA_TRACKING_ID}`} strategy="afterInteractive" />
-        <Script id="ga" defer strategy="afterInteractive">
-          {`
+const GoogleAnalytics = () => {
+  return (
+    <>
+      {existsGaId && (
+        <>
+          <Script
+            defer
+            src={`https://www.googletagmanager.com/gtag/js?id=${GA_TRACKING_ID}`}
+            strategy="afterInteractive"
+          />
+          <Script id="ga" defer strategy="afterInteractive">
+            {`
               window.dataLayer = window.dataLayer || [];
               function gtag(){dataLayer.push(arguments);}
               gtag('js', new Date());    
               gtag('config', '${GA_TRACKING_ID}');
           `}
-        </Script>
-      </>
-    )}
-  </>
-)
-
+          </Script>
+        </>
+      )}
+    </>
+  )
+}
 export default GoogleAnalytics
